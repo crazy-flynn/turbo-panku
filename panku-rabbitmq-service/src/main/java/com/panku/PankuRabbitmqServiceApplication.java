5b563af616be5c9eac937f583bfdd80c84635e8a
crazy-coder <flynn_zhang@aliyun.com>
2021-07-09 16:45:57 +0800

    feature(rabbitMQ 学习记录): rabbitMQ 学习记录
    Description: rabbitmq整合springboot

---
+++ panku-rabbitmq-service/src/main/java/com/panku/PankuRabbitmqServiceApplication.java
@@ -0,0 +1,13 @@
+package com.panku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PankuRabbitmqServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PankuRabbitmqServiceApplication.class, args);
+    }
+
+}
